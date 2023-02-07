0a05e09629fa31dc0fa39cf7bb4437548ae7ef15
Nihat <nihattagizade2003@gmail.com>
2023-02-07 20:57:07 +0400

    nnn

---
+++ 07022023home/07022023home/Notebook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07022023home
+{
+    internal class Notebook
+    {
+
+        public Notebook(string brand, string model)
+        {
+            Brand = brand;
+            Model = model;
+        }
+
+        public Notebook(string brand, string model, double price) : this(brand, model)
+        {
+            Price = price;
+        }
+
+        public Notebook(string brand, string model, double price, int ram) : this(brand, model, price)
+        {
+            RAM = ram;
+        }
+
+        public string Brand;
+        public string Model;
+        public double Price;
+        public int RAM = 4;
+
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Brand: {Brand} - Model: {Model} - Price: {Price}");
+        }
+
+    }
+}
---
+++ 07022023home/07022023home/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _07022023home
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var nt1 = new Notebook("Asus", "Rog", 1200, 16);
+
+            Console.WriteLine("Notebook count:");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            Notebook[] notebooks = new Notebook[count];
+
+           for (int i = 0; i < count; i++)
+            {
+                Console.Write("Brand: ");
+                string brand = Console.ReadLine();
+                Console.Write("Model: ");
+                string model = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = Convert.ToDouble(Console.ReadLine());
+                Console.Write("RAM: ");
+                int ram = Convert.ToInt32(Console.ReadLine());
+
+                Notebook nt = new Notebook(brand, model) { Price = price, RAM = ram };
+                notebooks[i] = nt;
+            }
+
+            double sum = 0;
+            for (int i = 0; i < notebooks.Length; i++)
+            {
+                sum += notebooks[i].Price;
+            }
+
+            var result = sum / notebooks.Length;
+            Console.WriteLine(result);
+
+        }
+    }
+}
